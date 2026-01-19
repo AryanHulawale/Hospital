6bf93d95c3fc528d35d419fcc875a400acaebbce
Aryan Hulawale <152802583+AryanHulawale@users.noreply.github.com>
2026-01-19 16:17:38 +0530

    Delete server directory

--- server/routes/appointmentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createAppointment, deleteAppointment, getAppointments } from '../controllers/appointmentController.js';
-const router = express.Router();
-
-router.get('/', getAppointments)
-router.post('/create', createAppointment)
-router.delete('/delete/:id', deleteAppointment)
-
-export default router;
--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { login,register,fetchMe } from '../controllers/authController.js';
-const router = express.Router();
-
-router.post('/login', login)
-router.post('/register', register)
-router.get('/me', fetchMe)
-
-export default router;
